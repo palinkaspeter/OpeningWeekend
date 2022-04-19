db06432f9c49902a9327e9b9cbe52854549819f6
palinkaspeter <96993856+palinkaspeter@users.noreply.github.com>
2022-04-19 17:23:18 +0200

    3.feladat

---
+++ OpeningWeekend/OpeningWeekend.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpeningWeekend
+{
+    public class OpeningWeekend
+    {
+        public string eredetiCim;
+        public string magyarCim;
+        public string bemutato;
+        public string forgalmazo;
+        public int bevel;
+        public int latogato;
+        public OpeningWeekend(string eredetiCim, string magyaCim, string bemuato, string forgalmazo, int bevel, int latogato) 
+        { 
+            this.eredetiCim = eredetiCim;
+            this.magyarCim = magyaCim;
+            this.bemutato = bemuato;
+            this.forgalmazo = forgalmazo;
+            this.bevel = bevel;
+            this.latogato = latogato;
+
+        }
+    }
+}
--- OpeningWeekend/Program.cs
+++ OpeningWeekend/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace OpeningWeekend
 {
@@ -10,6 +11,25 @@ namespace OpeningWeekend
     {
         static void Main(string[] args)
         {
-        }
+            List<OpeningWeekend> filmek = new List<OpeningWeekend>();
+            string[] sorok = File.ReadAllLines("nyitohetvege.txt");
+            sorok=sorok.Skip(1).ToArray();
+            foreach (string sorokLine in sorok) 
+            {
+                string[] sor = sorokLine.Split(';');
+                OpeningWeekend nyitohetvege = new OpeningWeekend(
+                    sor[0],
+                    sor[1],
+                    sor[2],
+                    sor[3],
+                    int.Parse(sor[4]), 
+                    int.Parse(sor[5])
+                    );
+                filmek.Add(nyitohetvege);
+                    }
+            //3. feladat
+            Console.WriteLine($"3.feladat: {sorok.Count()}");
+            Console.ReadKey();
+        }   
     }
 }
